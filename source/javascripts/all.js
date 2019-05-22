b5d1530071f50f3ee9b56c8b90143fc77ebdf67e
Lazared <eduard.enl@gmail.com>
2019-05-22 23:16:49 +0100

    Attempted to fix the all.js issue and navigation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,7 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+//= require ./all_nosearch
+//= require ./app/_search
+/* document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('code.highlighted').forEach((block) => {
         hljs.highlightBlock(block);
     });
-});
+}); */
