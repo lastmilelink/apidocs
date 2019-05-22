fb54d2f95208ee768a83a87ef401ce2df6d71df3
Lazared <eduard.enl@gmail.com>
2019-05-22 23:06:10 +0100

    Multiple changes , images , CSS, colapsable panels, code verbs and http codes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('code.highlighted').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+});
